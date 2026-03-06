c48c28ddf8177d1474a4b5704a512872074643ad
Perepechko Aleksey <elf.ast.ahe@gmail.com>
2026-03-06 12:38:16 +0300

    add migrations

--- migrations/tiny-auth-service/0001_table_users.go
+++ migrations/tiny-auth-service/0001_table_users.go
@@ -1,1 +1,91 @@
 package tiny_auth_service
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/ElfAstAhe/go-service-template/pkg/errs"
+	"github.com/pressly/goose/v3"
+)
+
+const (
+	sqlCreateTableUsers string = `
+create table if not exists users (
+    id varchar(50) not null,
+    name varchar(100) not null,
+    password_hash varchar(100) not null,
+    active bool null default true,
+    deleted bool null default false,
+    created_at timestamptz default now(),
+    updated_at timestamptz default now(),
+    constraint users_pk primary key (id),
+    constraint users_uk unique (name)
+)
+`
+	sqlDropTableUsers string = `drop table if exists users`
+
+	sqlCreateIndexUsersAlive string = `create index if not exists idx_users_alive on users (deleted asc, active desc, name asc)`
+	sqlDropIndexUsersAlive   string = `drop index if exists idx_users_alive`
+)
+
+func up0001(ctx context.Context, db *sql.DB) error {
+	if err := createTableUsers(ctx, db); err != nil {
+		return err
+	}
+	if err := createIndexUsersAlive(ctx, db); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func createTableUsers(ctx context.Context, db *sql.DB) error {
+	_, err := db.ExecContext(ctx, sqlCreateTableUsers)
+	if err != nil {
+		return errs.NewDBMigrationError("create table users", err)
+	}
+
+	return nil
+}
+
+func createIndexUsersAlive(ctx context.Context, db *sql.DB) error {
+	_, err := db.ExecContext(ctx, sqlCreateIndexUsersAlive)
+	if err != nil {
+		return errs.NewDBMigrationError("create index users alive", err)
+	}
+
+	return nil
+}
+
+func down0001(ctx context.Context, db *sql.DB) error {
+	if err := dropIndexUsersAlive(ctx, db); err != nil {
+		return err
+	}
+	if err := dropTableUsers(ctx, db); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func dropTableUsers(ctx context.Context, db *sql.DB) error {
+	_, err := db.ExecContext(ctx, sqlDropTableUsers)
+	if err != nil {
+		return errs.NewDBMigrationError("drop table users", err)
+	}
+
+	return nil
+}
+
+func dropIndexUsersAlive(ctx context.Context, db *sql.DB) error {
+	_, err := db.ExecContext(ctx, sqlDropIndexUsersAlive)
+	if err != nil {
+		return errs.NewDBMigrationError("drop index users alive", err)
+	}
+
+	return nil
+}
+
+func init() {
+	goose.AddMigrationNoTxContext(up0001, down0001)
+}
--- migrations/tiny-auth-service/0003_table_user_roles.go
+++ migrations/tiny-auth-service/0003_table_user_roles.go
@@ -1,1 +1,90 @@
 package tiny_auth_service
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/ElfAstAhe/go-service-template/pkg/errs"
+	"github.com/pressly/goose/v3"
+)
+
+const (
+	sqlCreateTableUserRoles string = `
+create table if not exists user_roles (
+    id varchar(50) not null,
+    user_id varchar(50) not null,
+    role_id varchar(50) not null,
+    created_at timestamptz null default now(),
+    constraint user_roles_pk primary key (id),
+    constraint user_roles_fk_user foreign key (user_id) references users (id),
+    constraint user_roles_fk_role foreign key (role_id) references roles (id),
+    constraint user_roles_uk unique (user_id, role_id)
+)
+`
+	sqlDropTableUserRoles string = `drop table if exists user_roles`
+
+	sqlCreateIndexUserRoles string = `create index if not exists idx_user_roles on user_roles(user_id asc, role_id asc)`
+	sqlDropIndexUserRoles   string = `drop index if exists idx_user_roles`
+)
+
+func up0003(ctx context.Context, db *sql.DB) error {
+	if err := createTableUserRoles(ctx, db); err != nil {
+		return errs.NewDBMigrationError("create user roles table", err)
+	}
+	if err := createIndexUserRoles(ctx, db); err != nil {
+		return errs.NewDBMigrationError("create user roles index", err)
+	}
+
+	return nil
+}
+
+func createTableUserRoles(ctx context.Context, db *sql.DB) error {
+	_, err := db.ExecContext(ctx, sqlCreateTableUserRoles)
+	if err != nil {
+		return errs.NewDBMigrationError("create user roles table", err)
+	}
+
+	return nil
+}
+
+func createIndexUserRoles(ctx context.Context, db *sql.DB) error {
+	_, err := db.ExecContext(ctx, sqlCreateIndexUserRoles)
+	if err != nil {
+		return errs.NewDBMigrationError("create user roles index", err)
+	}
+
+	return nil
+}
+
+func down0003(ctx context.Context, db *sql.DB) error {
+	if err := dropIndexUserRoles(ctx, db); err != nil {
+		return errs.NewDBMigrationError("drop user roles index", err)
+	}
+	if err := dropTableUserRoles(ctx, db); err != nil {
+		return errs.NewDBMigrationError("drop user roles table", err)
+	}
+
+	return nil
+}
+
+func dropTableUserRoles(ctx context.Context, db *sql.DB) error {
+	_, err := db.ExecContext(ctx, sqlDropTableUserRoles)
+	if err != nil {
+		return errs.NewDBMigrationError("drop user roles table", err)
+	}
+
+	return nil
+}
+
+func dropIndexUserRoles(ctx context.Context, db *sql.DB) error {
+	_, err := db.ExecContext(ctx, sqlDropIndexUserRoles)
+	if err != nil {
+		return errs.NewDBMigrationError("drop user roles index", err)
+	}
+
+	return nil
+}
+
+func init() {
+	goose.AddMigrationNoTxContext(up0003, down0003)
+}
