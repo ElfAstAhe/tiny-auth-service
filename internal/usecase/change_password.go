12d793446a0510770f909016aae5d9a48bb89d32
Perepechko Aleksey <elf.ast.ahe@gmail.com>
2026-03-14 23:36:32 +0300

    change keys and password use cases implemented, profile fixed, add blank facades

---
+++ internal/usecase/change_password.go
@@ -0,0 +1,123 @@
+package usecase
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+
+	usecase "github.com/ElfAstAhe/go-service-template/pkg/db"
+	"github.com/ElfAstAhe/go-service-template/pkg/errs"
+	"github.com/ElfAstAhe/go-service-template/pkg/utils"
+	"github.com/ElfAstAhe/tiny-auth-service/internal/domain"
+	domerrs "github.com/ElfAstAhe/tiny-auth-service/internal/domain/errs"
+)
+
+type ChangePasswordUseCase interface {
+	ChangePassword(ctx context.Context, userID, oldPassword, newPassword string) error
+}
+
+type ChangePasswordInteractor struct {
+	hashHelper utils.Cipher
+	tm         usecase.TransactionManager
+	userRepo   domain.UserRepository
+}
+
+func NewChangePasswordUseCase(hashHelper utils.Cipher, tm usecase.TransactionManager, userRepo domain.UserRepository) *ChangePasswordInteractor {
+	return &ChangePasswordInteractor{
+		hashHelper: hashHelper,
+		tm:         tm,
+		userRepo:   userRepo,
+	}
+}
+
+func (cp *ChangePasswordInteractor) ChangePassword(ctx context.Context, userID, oldPassword, newPassword string) error {
+	if err := cp.validate(userID, oldPassword, newPassword); err != nil {
+		return domerrs.NewBllValidateError("ChangePasswordInteractor.ChangePassword", "validate income data failed", err)
+	}
+
+	err := cp.tm.WithinTransaction(ctx, nil, func(ctx context.Context) error {
+		// пользователь
+		user, err := cp.userRepo.Find(ctx, userID)
+		if err != nil {
+			return err
+		}
+		// хэш сумма новый пароль
+		newPasswordHash, err := cp.hashHelper.EncryptString(newPassword)
+		if err != nil {
+			return domerrs.NewBllError("ChangePasswordInteractor.ChangePassword", "new password hash build failed", err)
+		}
+		// хэш сумма старый пароль
+		oldPasswordHash, err := cp.hashHelper.EncryptString(oldPassword)
+		if err != nil {
+			return domerrs.NewBllError("ChangePasswordInteractor.ChangePassword", "old password hash build failed", err)
+		}
+		// проверки
+		err = cp.validatePassword(oldPasswordHash, newPasswordHash, user)
+		if err != nil {
+			return err
+		}
+
+		// storing
+		user.PasswordHash = newPasswordHash
+		user.UpdatedAt = time.Now()
+
+		_, err = cp.userRepo.Change(ctx, user)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		if errors.As(err, new(*errs.DalNotFoundError)) {
+			return domerrs.NewBllNotFoundError("ChangePasswordInteractor.ChangePassword", "User", userID, err)
+		}
+
+		return domerrs.NewBllError("ChangePasswordInteractor.ChangePassword", fmt.Sprintf("user id [%v] change password failed", userID), err)
+	}
+
+	return nil
+}
+
+func (cp *ChangePasswordInteractor) validate(userID, oldPassword, newPassword string) error {
+	if strings.TrimSpace(userID) == "" {
+		return errs.NewInvalidArgumentError("userID", "user id required")
+	}
+	// * empty
+	if strings.TrimSpace(newPassword) == "" {
+		return errs.NewInvalidArgumentError("newPassword", "new password required")
+	}
+	// * empty
+	if strings.TrimSpace(oldPassword) == "" {
+		return errs.NewInvalidArgumentError("oldPassword", "old password required")
+	}
+
+	return nil
+}
+
+// validatePassword
+// сюда можно добавить бизнес логику на проверку пароля
+//   - strong password
+//   - same password
+//   - history password
+//   - min length
+//   - etc
+//
+// реализовываем простые проверки
+//   - empty
+//   - same password
+//   - old and current password match
+func (cp *ChangePasswordInteractor) validatePassword(oldPasswordHash, newPasswordHash string, user *domain.User) error {
+	// * same password
+	if newPasswordHash == user.PasswordHash {
+		return domerrs.NewBllValidateError("ChangePasswordInteractor.validatePassword", "new password same as current old password", nil)
+	}
+	// * old and current password match
+	if oldPasswordHash != user.PasswordHash {
+		return domerrs.NewBllValidateError("ChangePasswordInteractor.validatePassword", "old password does not match current password", nil)
+	}
+
+	return nil
+}
--- internal/usecase/profile.go
+++ internal/usecase/profile.go
@@ -2,10 +2,13 @@ package usecase
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"strings"
 
 	"github.com/ElfAstAhe/go-service-template/pkg/errs"
 	"github.com/ElfAstAhe/tiny-auth-service/internal/domain"
+	domerrs "github.com/ElfAstAhe/tiny-auth-service/internal/domain/errs"
 )
 
 type ProfileUseCase struct {
@@ -23,9 +26,14 @@ func (p *ProfileUseCase) Get(ctx context.Context, username string) (*domain.User
 		return nil, errs.NewInvalidArgumentError("username", "must not be empty")
 	}
 
-	return p.buildAnswer(ctx, username)
-}
+	res, err := p.userRepo.FindByName(ctx, username)
+	if err != nil {
+		if errors.As(err, new(*errs.DalNotFoundError)) {
+			return nil, domerrs.NewBllNotFoundError("ProfileUseCase.Get", "User", username, err)
+		}
 
-func (p *ProfileUseCase) buildAnswer(ctx context.Context, username string) (*domain.User, error) {
+		return nil, domerrs.NewBllError("ProfileUseCase.Get", fmt.Sprintf("get user name [%v] failed", username), err)
+	}
 
+	return res, nil
 }
